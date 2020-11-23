bcd7a433e77690c55228bf57b822d871ae953ece
humenmiku <71753428+humenmiku@users.noreply.github.com>
2020-11-23 15:05:09 +0800

    Update demo.java

--- add/src/main/java/com/yunlong/demo.java
+++ add/src/main/java/com/yunlong/demo.java
@@ -2,7 +2,7 @@ package com.yunlong;
 
 public class demo {
    public static void main(String[] args) {
-        System.out.println("git更新maven代码");
+      
        System.out.println("2");
        System.out.println("idea更新github仓库");
     }
