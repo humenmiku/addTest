4e5aad5cedc9e9f95752ed83808bac36ae46c713
牛客198518325号 <gkdxdmaoligei@163.com>
2020-11-23 15:04:48 +0800

    commit

--- add/src/main/java/com/yunlong/demo.java
+++ add/src/main/java/com/yunlong/demo.java
@@ -4,5 +4,6 @@ public class demo {
    public static void main(String[] args) {
         System.out.println("git更新maven代码");
        System.out.println("2");
+       System.out.println("idea更新github仓库");
     }
 }
